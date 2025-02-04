3a7bf576c4fefbf52cd71dbd09aab414069188e9
Amarachukwu Onuoha <amarachukwuonuohabars007@gmail.com>
2025-02-04 10:01:55 +0100

    more boyish

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
+    "Elliot🙄",
+    " pleasee...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
